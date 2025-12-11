01fe263084cd8af0a06259ab145c9984f562b6c2
Shyam Kumar <shyamprajapatiofficial2011@gmail.com>
2025-12-11 18:06:12 +0530

    wait groups done

--- 20_goRoutines/main.go
+++ 20_goRoutines/main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
+	// "time"
 )
 
-func task(i int) {
+func task(i int, w* sync.WaitGroup) {
+	defer w.Done()  //yha par defer keyword ki used to tell ki ye kaam bilkul end mai krna hai
 	fmt.Println(i)
 }
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 0; i < 10;i++ {   
+		wg.Add(1)
 		// task(i)  //ye hai aam zindigi using a single core thread
 		//now using go rounting to make it non blocking concurrent, mentos zindigi
-		go task(i)
+		go task(i, &wg)
 	}
 
+	wg.Wait()
 	//yha par hume main func to toda rok kar rkhna pad rha hai so that ye func pura exit na ho jaygea
-	time.Sleep(time.Second*2) //boom 
+	// time.Sleep(time.Second*2) //boom 
+	//we don't use sleep wala method in real use kuki hume nh pta kitne der rokna hai, therefore comes the concept of waitgroup, joki sync package se aata hai, isme 3 methods yaad rkhne hai, Add(), Done(), Wait()
+
 }
